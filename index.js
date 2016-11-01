196201e215d2c6a88c74cda8c7c98c62da160eea
Haydn Ewers <haydn@particlesystem.com>
2016-11-01 11:09:01 +1100

    Improvements

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const REG_EXP = /^\s*[\(\[]\s*([\.0-9\?]+)\s*[\]\)]\s*(.*)$/i;
+const REG_EXP = /^\s*\#[0-9]+\s*[\(\[]\s*([\.0-9\?]+)\s*[\]\)](.*)$/i;
 
 function update() {
 
   const cards = Array.from(document.querySelectorAll('#board .list-card'));
-  const lists = Array.from(document.querySelectorAll('#board .list'));
+  const lists = Array.from(document.querySelectorAll('#board .list-wrapper:not(.js-add-list)'));
 
   cards.forEach((card) => {
     const title = card.querySelector('.list-card-title');
+    const hiddenId = title.querySelector('.card-short-id.hide');
+
+    if (hiddenId) {
+      hiddenId.classList.remove('hide');
+      hiddenId.classList.add('subtle-id');
+    }
+
     const badgeList = card.querySelector('.badges');
     const match = title.innerText.match(REG_EXP);
 
@@ -28,10 +35,34 @@ function update() {
       else badge.classList.remove('point-badge--unestimated');
 
       title.childNodes[1].data = match[2];
+    } else if (title.childNodes[1].data[0] !== ' ') {
+      title.childNodes[1].data = ' ' + title.childNodes[1].data;
     }
+
   });
 
   lists.forEach((list) => {
+    const listHeaderName = list.querySelector('.list-header-name');
+    const listTitle = listHeaderName ? listHeaderName.value.trim().toLowerCase() : '';
+
+    [
+      'inbox',
+      'current sprint',
+    ].forEach((title) => {
+      if (listTitle === title) {
+        list.classList.add('list-wrapper-start');
+      }
+    });
+
+    [
+      'next',
+      'merged',
+    ].forEach((title) => {
+      if (listTitle === title) {
+        list.classList.add('list-wrapper-end');
+      }
+    });
+
     const listHeaderExtras = list.querySelector('.list-header-extras');
     const cardBadges = Array.from(list.querySelectorAll('.list-cards .point-badge'));
     const points = cardBadges.reduce((points, b) => {
